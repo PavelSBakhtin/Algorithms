c33569a6360c7bd424e1deea4d754e5e8425f452
PavelSB <1987PavelS@mail.ru>
2023-05-25 16:10:11 +0300

    lesson 2 task 2 done

--- Lessons/lesson2_task2.py
+++ Lessons/lesson2_task2.py
@@ -4,7 +4,7 @@ import random
 
 def randNums():
     a = []
-    l = random.randint(5, 7)
+    l = random.randint(6, 9)
     for i in range(l):
         k = random.randint(1, 9)
         r = k
@@ -16,4 +16,29 @@ def randNums():
         i += 1
     return a
 
+def quickSort(arr, sPos, ePos):
+    leftPos = sPos
+    rightPos = ePos
+    insex = int((sPos + ePos) / 2)
+    pivot = arr[insex]
+    while True:
+        while arr[leftPos] < pivot:
+            leftPos += 1
+        while arr[rightPos] > pivot:
+            rightPos -= 1
+        if leftPos <= rightPos:
+            if leftPos < rightPos:
+                arr[leftPos], arr[rightPos] = arr[rightPos], arr[leftPos]
+            leftPos += 1
+            rightPos -= 1
+        if leftPos > rightPos:
+            break
+    if leftPos < ePos:
+        quickSort(arr, leftPos, ePos)
+    if sPos < rightPos:
+        quickSort(arr, sPos, rightPos)
+    return arr
 
+b = randNums()
+print(b)
+print(quickSort(b, 0, len(b) - 1))
